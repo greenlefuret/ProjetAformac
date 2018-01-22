4e0525855189d0e3ecbc8a7c615e2a69b508c282
greenLeradounaud <greenleradounaud@gmail.com>
2018-01-22 16:46:56 +0100

    tete

---
+++ Projet1/php_test_formulaire/index.php
@@ -0,0 +1,19 @@
+<body>
+
+   <form class="" action="pages/cible.php" method="post">
+     <p>E-mail</p>
+     <input type="email" name="mail" value="" method="post"></br>
+     <p>Telephone</p>
+     <input type="input" name="telephone" value="" method="post"></br>
+     <p>Nom</p>
+     <input type="input" name="nom" value="" method="post"></br>
+     <p>Age</p>
+     <input type="input" name="age" value="" method="post"></br>
+     <p>Prenom</p>
+     <input type="input" name="prenom" value="" method="post"></br>
+
+     <input type="submit" value="Valider" />
+     <p>Appuyer pour envoyer le mail toto.</p>
+   </form>
+
+ </body>
