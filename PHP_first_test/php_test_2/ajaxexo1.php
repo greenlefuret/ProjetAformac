1037d9665d97556fbd3fdd5e9aff60fa3f3144d4
greenLeradounaud <greenleradounaud@gmail.com>
2018-01-26 09:20:56 +0100

    push final

---
+++ PHP_first_test/php_test_2/ajaxexo1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <title>Ajax Test</title>
+    <link rel="stylesheet" href="css/ajaxexo1.css">
+    <script src="js/script.js"></script>
+  </head>
+  <body>
+    <h2>Ajax</h2>
+    <form class="formulaire" method="post">
+      <div class="return">
+      </div>
+      <input type="text" placeholder="Votre nom" class="nom"></br>
+      <textarea class="message" placeholder="Votre Message"></textarea></br>
+      <input type="submit" value="Envoyer" class="submit">
+    </form>
+    <div class="afficher">
+    </div>
+
+  </body>
+</html>
